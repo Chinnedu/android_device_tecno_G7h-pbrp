833a07d539df964db71b3b4579d63ac6c75e25e4
infinixs5lite <kelvinchinedu97@gmail.com>
2023-01-05 12:44:27 +0100

    Update omni_TECNO-KG7h.mk

--- omni_TECNO-KG7h.mk
+++ omni_TECNO-KG7h.mk
@@ -1,25 +1,41 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := KG7h
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from TECNO-KG7h device
-$(call inherit-product, device/tecno/TECNO-KG7h/device.mk)
+# Inherit from KG7h device
+$(call inherit-product, device/realme/KG7h/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/pb/config/common.mk)
 
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := TECNO-KG7h
-PRODUCT_NAME := omni_TECNO-KG7h
-PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := TECNO KG7h
+PRODUCT_DEVICE := KG7h
+PRODUCT_NAME := omni_KG7h
+PRODUCT_BRAND := tecno
+PRODUCT_MODEL := Tecno Spark 8P
 PRODUCT_MANUFACTURER := tecno
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bootimage.build.date.utc=0
