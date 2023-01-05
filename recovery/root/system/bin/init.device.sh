52b1fc31657258ffde39e7ced73c27eb5fba1e68
infinixs5lite <kelvinchinedu97@gmail.com>
2023-01-05 14:06:35 +0100

    Delete init.device.sh

--- recovery/root/system/bin/init.device.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-prjVersion=$(cat /proc/oplusVersion/prjVersion)
-if [ "$prjVersion" = 20682 ]; then
-  resetprop ro.build.product KG7h
-  resetprop ro.product.device KG7h
-  resetprop ro.product.model KG7h
-fi
